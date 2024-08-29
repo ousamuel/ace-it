1fe86c36a46f4bb9891cd45c3fbd369cd8fe7a24
T-Godwins <gtuyishime@gmail.com>
2024-08-29 18:04:25 -0400

    api key

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { OpenAI } from 'openai';
 
 const openai = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
+  apiKey: process.env.OPENAI_KEY,
 });
 
 const systemPrompt = `
