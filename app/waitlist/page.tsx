ae555891fceb6311f386aa13ea9fcc84766f0c66
Samuel <samuelou510@gmail.com>
2024-09-01 19:33:07 -0400

    fix waitlist styling

--- app/waitlist/page.tsx
+++ app/waitlist/page.tsx
@@ -7,6 +7,10 @@ import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { EnvVarWarning } from "@/components/env-var-warning";
+import HeaderAuth from "@/components/header-auth";
+
 export default async function Waitlist({
   searchParams,
 }: {
@@ -29,8 +33,19 @@ export default async function Waitlist({
   }
 
   return (
-    <div className="my-20 flex-1 w-full flex flex-col gap-12 text-center items-center px-3">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <div className="flex-1 w-full flex flex-col gap-12 text-center items-center px-3">
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 ">
+        <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-3xl">
+          <div className="flex gap-5 items-center font-semibold">
+            <Link href="/" className="flex flex-row gap-1">
+              <h1>ACE</h1>
+              <h1 className="text-green-500">IT</h1>
+            </Link>
+          </div>
+          <HeaderAuth />
+        </div>
+      </nav>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl my-4">
         Thanks for joining our waitlist!
       </h1>
       <h3 className="sm:w-3/5 scroll-m-20 text-2xl font-semibold tracking-tight">
